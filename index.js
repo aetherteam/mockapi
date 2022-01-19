a1db4c97d5e467c5fdb5a6efadd7682f8440b067
Mem U Sins <83538724+memUsins@users.noreply.github.com>
2022-01-20 01:00:40 +0300

    Create temp nickname's dataset and get method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import Mock from './src/mock.js'
 
-const ids = new Mock({id: {type: "intid"}})
+const ids = new Mock({id: {type: "intid"}, nickname: {type: "nickname"}})
 const users = ids.get(2);
 
 console.log(users)
--- src/generate/index.js
+++ src/generate/index.js
@@ -1,9 +1,11 @@
+import dataNicknames from './dataset/nicknames.js'
+
 function generate(object, count) {
-	if (!object.type) throw new Error("Field type is not defined"); 
+	if (!object.type) throw new Error("Field type is not defined");
 	if (!generators[object.type]) throw new TypeError("Using unexisted type " + object.type);
 
 	let generator = generators[object.type];
-	
+
 	if (count === 1) return generator();
 
 	const result = [];
@@ -23,6 +25,9 @@ function generate(object, count) {
 const generators = {
 	intid: () => {
 		return 1000000 + Math.floor(Math.random() * 1000);
+	},
+	nickname: () => {
+		return dataNicknames[Math.floor(Math.random() * dataNicknames.length)]
 	}
 }
 export default generate;
