73a2e91d5a8b9f15543e53ef1b302a6e053edec6
EugeneKannou <42428147+EugeneKannou@users.noreply.github.com>
2022-01-20 03:03:06 +0500

    Readme added

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # mockapi
+## Installation
+
+`npm i mockapi`
+
+## Usage
+
+```js
+import mockapi as Mock;
+
+const userTemplate = {
+    id: {
+        type: "intid",
+        unique: true
+    },
+    name: {
+        type: "fullname"
+    }
+}
+
+// create template (explained below)
+
+const userMock = new Mock(userTemplate); // create instance of Mock from template
+
+const users = userMock.get(10); // generate 10 users
+```
+
+### Templates 
+
+```js
+
+```
--- index.js
+++ index.js
@@ -1,6 +1,44 @@
-import Mock from './src/mock.js'
+import generate from './generate/index.js';
 
-const ids = new Mock({id: {type: "intid"}, nickname: {type: "nickname"}})
-const users = ids.get(2);
+class Mock {
+    constructor(template) {
+        if (Object.keys(template).length < 1) throw new Error("Template must have at least one key");
+        this.template = template;
+    }
 
-console.log(users)
+    get(count) {
+        let template = Object.entries(this.template);
+
+        if (count < 1) {
+            throw new Error("You should generate at least one item (or else why do you need to call this method? :) ))");
+        } else if (count === 1) {
+            let generated = {}
+            template.forEach((item) => {
+                generated[item[0]] = generate(item[1], 1);
+            });
+
+            return generated;
+        } else {
+            let generated = {}
+            template.forEach((item) => {
+                generated[item[0]] = generate(item[1], count);
+            });
+
+            const generatedArr = Object.entries(generated)
+            const result = []
+            for (let i = 0; i < count; i++) {
+                let record = {}
+
+                generatedArr.forEach((item) => {
+                    record[item[0]] = item[1][i];
+                });
+
+                result.push(record)
+            }
+
+            return result;
+        }
+    }
+
+}
+export default Mock;
