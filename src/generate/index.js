39b3ec3bb5069d141d876934ed57b7070abe4e68
Mem U Sins <83538724+memUsins@users.noreply.github.com>
2022-01-19 23:37:20 +0300

    Create temp id generate function

---
+++ src/generate/index.js
@@ -0,0 +1,21 @@
+class Generate {
+	constructor() { }
+
+	generateID(count) {
+		let idsArray = [];
+
+		const gen = () => {
+			return 1000000 + Math.floor(Math.random() * (count * 10));
+		}
+
+		for (let i = 1; i <= count; i++) {
+			let result = gen()
+			
+			do {
+				idsArray.push(result)
+			} while (!idsArray.includes(result))
+		}
+
+		return idsArray;
+	}
+}
